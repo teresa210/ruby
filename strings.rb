dcbb1aaec44852325a0c28b5398e15bd7a9de5b7
teresa210 <teresa210@gmail.com>
2020-04-12 15:05:47 -0700

    strings.rb

---
+++ strings.rb
@@ -0,0 +1,13 @@
+class Adam
+  
+    def how_old(age)
+      puts "#{age}".upcase
+      puts "#{age}".empty?
+    end
+
+    def birthday(month, day)
+        puts "#{month} #{10}".capitalize
+    end
+
+  
+  end
