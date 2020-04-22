05c102e1baff69a7fec121286930e9dafe60c6a9
teresa210 <teresa210@gmail.com>
2020-04-22 15:47:04 -0700

    updated

--- strings.rb
+++ strings.rb
@@ -13,12 +13,28 @@ class Adam
 
     def birthday(month, day)
         puts "#{month} #{day}".capitalize!
+    # ! is a bang and it will change the object to the output instead of just changing the output
     end
+
+#GSUB
 
     def say_hello(name)
         puts "Hello #{name}".gsub("adam", "Adam Wu")
+    # if name is adam then it will put adam wu, if not then it wont change
     end
 
-    # ! is a bang and it will change the object to the output instead of just changing the output
+#SLICE
+
+    def friends(name)
+        puts "#{name}".slice(2)
+        # gives the letter at the given index
+        puts "#{name}".slice(3, 2)
+        # gives the 2 letters from the 3rd spot
+        puts "#{name}".slice(1..2) 
+    end
+
+#CHOP
+
+    def
   
   end
