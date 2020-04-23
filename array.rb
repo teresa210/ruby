58faa5fd6ac69795bcfda94356b6019c9de6f8df
teresa210 <teresa210@gmail.com>
2020-04-23 16:50:55 -0700

    updated

--- array.rb
+++ array.rb
@@ -1,12 +1,22 @@
 class Team
+
     def
-        initialize(members)
+        initialize
         @members = []
     end
 
-    def cs_team(name)
-        @members.push "#{name}"
+    def team
+      @members
+    end
+    
+    def add(name)
+      @members << name
+    end
+
+    def find
+      @members.select {|names| names.start_with? "t"}
     end
 
 end
 
+#select, map, all?, any?, each
