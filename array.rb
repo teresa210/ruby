c30d099058b13ecab8e5a354dcc14ecea14b6aee
teresa210 <teresa210@gmail.com>
2020-04-23 21:48:37 -0700

    updated

--- array.rb
+++ array.rb
@@ -13,9 +13,17 @@ class Team
       @members << name
     end
 
-    def find
-      @members.select {|names| names.start_with? "t"}
+    def find(x)
+        @members.select {|names| names.start_with? "#{x}"}
+        #method returns names in array that start with whatever the paramater is
+        @members.all? { |names| names.length >= 4 }
+        #method returns true if the block never returns false or nil
+        @members.all? { |names| names.length >= 7 }
+        #method returns true if any part of the block is true
     end
+
+
+
 
 end
 
